e41a496d3ef4a66cfa1097eb7543d568fb1016d4
PierreChemin <72612513+PierreChemin@users.noreply.github.com>
2020-11-27 17:12:45 +0100

    Update Huffman_tree.c
    Part of Pierre Chemin :
    here we can find the functions about the huffman tree creation and all the sub functions like the delate, the add and the sum function

--- Huffman_tree.c
+++ Huffman_tree.c
@@ -21,7 +21,7 @@ List* create_list_from_Node(Node*n){
     return l;
 }
 
-void add_List(List** list,Node* new_node){
+void add_List(List** list,Node* new_node){  
     if((*list)==NULL){
         (*list) = create_list_from_Node(new_node);
     }else{
@@ -33,7 +33,7 @@ void add_List(List** list,Node* new_node){
     }
 }
 
-void delate_List(List** list,Node* min){
+void delate_List(List** list,Node* min){ //to delate a node in the list i use a double pointer by this way i can easily delate the old node
     if(*list!=NULL){
         List** temp=list;
         while(*temp!=NULL){
@@ -59,24 +59,24 @@ Node* min_list(List* list){
     }
 }
 
-int sum_LF(List* list_nodes){
+int sum_LF(List* list_nodes){ // to get the size of a list of nodes
     if(list_nodes!=NULL) return sum_LF(list_nodes->next)+1;
 }
 
 
 void Huffman(List** list_nodes){
     if(*list_nodes!=NULL){
-        while(sum_LF(*list_nodes)>1){
+        while(sum_LF(*list_nodes)>1){ // For this part i just say that we continue the loop while in the list there is more than one node because at the end the only node will be the Huffman tree
             Node* min1 = min_list(*list_nodes);
             delate_List(list_nodes, min1);
             Node* min2 = min_list(*list_nodes);
-            delate_List(list_nodes,min2);
-            Node* new_node = (Node*)malloc(sizeof(Node));
+            delate_List(list_nodes,min2); // I first take the two minimum node of the list and delate them 
+            Node* new_node = (Node*)malloc(sizeof(Node)); // I initialize a new node which is the parent node of the the two previous nodes.
             new_node->occurence = min1->occurence + min2->occurence;
             new_node->caractere;
             new_node->right = min1;
             new_node->left = min2;
-            add_List(list_nodes,new_node);
+            add_List(list_nodes,new_node); // at the end i just add in the new list the parent node which will be the parent node 
         }
     }
 }
